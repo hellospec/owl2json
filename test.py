6e19ce972f2a181839b4286b1f8676dcdd7de1e5
htoomyataung <htoomyatag@hellospec.com>
2023-08-18 13:27:55 +0700

    Test case modified to test func instead of output result

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 import unittest
-import json
+from owlready2 import get_ontology
+from app import extract_nodes_and_links
 
-class TestNumber(unittest.TestCase):
+class TestExtractNodesAndLinks(unittest.TestCase):
     source = 'LearningObjectives (1145220_LO9)'
     expected_link_names = ["isabletoAnalyzeProblemOf", 
                            "isabletoApplyKnowledgeOf",
                            "isabletoUnderstandKnowledgeOf"]
-    
+
     @classmethod
     def setUpClass(cls):
-        with open('data/output.json', 'r') as json_file:
-            cls.data = json.load(json_file)
+        cls.ontology = get_ontology("file://data/example.owl").load()
+        cls.nodes, cls.links = extract_nodes_and_links(cls.ontology)
 
     def test_number_of_LearningObjectives(self):
-        # Count the number of source
-        count = sum(1 for entry in self.data["links"] if entry["source"] == self.source)
-        # Ensure the count matches the expected value
-        self.assertEqual(3, count)
+        count = sum(1 for entry in self.links if entry["source"] == self.source)     
+        # Ensure the count matches the expected value 
+        self.assertEqual(count, 3) 
 
     def test_match_LinkNames(self):
-        for entry in self.data["links"]:
+        for entry in self.links:
             if entry["source"] == self.source:
                 # Check if the link name is in the expected list
-                self.assertIn(entry["name"], self.expected_link_names)
-  
+                self.assertIn(entry["name"], self.expected_link_names) 
+
 if __name__ == '__main__':
     unittest.main()
