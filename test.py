745a771644f853132f42b8bca920c40e6112cde9
preecha <preechac@hellospec.com>
2023-09-09 03:51:47 +0700

    Add test for owl 2 json converter

--- test.py
+++ test.py
@@ -1,28 +1,70 @@
 import unittest
 from owlready2 import get_ontology
-from app import extract_nodes_and_links
+from app import extract_nodes_and_links, load_ontology
 
 class TestExtractNodesAndLinks(unittest.TestCase):
-    source = 'LearningObjectives (1145220_LO9)'
-    expected_link_names = ["isabletoAnalyzeProblemOf", 
-                           "isabletoApplyKnowledgeOf",
-                           "isabletoUnderstandKnowledgeOf"]
+    def test_match_convert_1141001_cource(self):
+        expected_json = {
+            "nodes": [
+                { "id": "1141001", "data": { "type": "courses" }},
+                { "id": "1141001_LO4", "data": { "type": "learning_objectives" }},
+                { "id": "1141001_LO3", "data": { "type": "learning_objectives" }},
+                { "id": "1141001_LO2", "data": { "type": "learning_objectives" }},
+                { "id": "1141001_LO1", "data": { "type": "learning_objectives" }},
+                { "id": "ระเบียบวิธีการทางสถิติ", "data": { "type": "topics" }},
+                { "id": "การสุ่มตัวอย่าง", "data": { "type": "topics" }},
+                { "id": "การทดสอบสมมติฐาน", "data": { "type": "topics" }},
+                { "id": "การประมาณค่าพารามิเตอร์", "data": { "type": "topics" }},
+                { "id": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "data": { "type": "topics" }},
+                { "id": "ระบบสมการเชิงเส้น", "data": { "type": "topics" }},
+                { "id": "รูปแบบมาร์คอฟ", "data": { "type": "topics" }}],
+            "edges": [
+                { "source": "1141001_LO1", "target": "ระเบียบวิธีการทางสถิติ", "data": { "name": "isabletoUnderstandKnowledgeOf", "title": "can_understand" }},
+                { "source": "1141001_LO1", "target": "การสุ่มตัวอย่าง", "data": { "name": "isabletoUnderstandKnowledgeOf", "title": "can_understand" }},
+                { "source": "1141001_LO1", "target": "การทดสอบสมมติฐาน", "data": { "name": "isabletoUnderstandKnowledgeOf", "title": "can_understand" }},
+                { "source": "1141001_LO1", "target": "การประมาณค่าพารามิเตอร์", "data": { "name": "isabletoUnderstandKnowledgeOf", "title": "can_understand" }},
+                { "source": "1141001_LO1", "target": "ระเบียบวิธีการทางสถิติ", "data": { "name": "isabletoApplyKnowledgeOf", "title": "can_apply" }},
+                { "source": "1141001_LO1", "target": "การสุ่มตัวอย่าง", "data": { "name": "isabletoApplyKnowledgeOf", "title": "can_apply" }},
+                { "source": "1141001_LO1", "target": "การทดสอบสมมติฐาน", "data": { "name": "isabletoApplyKnowledgeOf", "title": "can_apply" }},
+                { "source": "1141001_LO1", "target": "การประมาณค่าพารามิเตอร์", "data": { "name": "isabletoApplyKnowledgeOf", "title": "can_apply" }},
+                { "source": "1141001_LO1", "target": "ระเบียบวิธีการทางสถิติ", "data": { "name": "isabletoAnalyzeProblemOf", "title": "can_analyze" }},
+                { "source": "1141001_LO1", "target": "การสุ่มตัวอย่าง", "data": { "name": "isabletoAnalyzeProblemOf", "title": "can_analyze" }},
+                { "source": "1141001_LO1", "target": "การทดสอบสมมติฐาน", "data": { "name": "isabletoAnalyzeProblemOf", "title": "can_analyze" }},
+                { "source": "1141001_LO1", "target": "การประมาณค่าพารามิเตอร์", "data": { "name": "isabletoAnalyzeProblemOf", "title": "can_analyze" }},
+                { "source": "1141001_LO2", "target": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "data": { "name": "isabletoUnderstandKnowledgeOf", "title": "can_understand" }},
+                { "source": "1141001_LO2", "target": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "data": { "name": "isabletoApplyKnowledgeOf", "title": "can_apply" }},
+                { "source": "1141001_LO2", "target": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "data": { "name": "isabletoAnalyzeProblemOf", "title": "can_analyze" }},
+                { "source": "1141001_LO3", "target": "ระบบสมการเชิงเส้น", "data": { "name": "isabletoUnderstandKnowledgeOf", "title": "can_understand" }},
+                { "source": "1141001_LO3", "target": "ระบบสมการเชิงเส้น", "data": { "name": "isabletoApplyKnowledgeOf", "title": "can_apply" }},
+                { "source": "1141001_LO3", "target": "ระบบสมการเชิงเส้น", "data": { "name": "isabletoAnalyzeProblemOf", "title": "can_analyze" }},
+                { "source": "1141001_LO4", "target": "รูปแบบมาร์คอฟ", "data": { "name": "isabletoUnderstandKnowledgeOf", "title": "can_understand" }},
+                { "source": "1141001_LO4", "target": "รูปแบบมาร์คอฟ", "data": { "name": "isabletoApplyKnowledgeOf", "title": "can_apply" }},
+                { "source": "1141001_LO4", "target": "รูปแบบมาร์คอฟ", "data": { "name": "isabletoAnalyzeProblemOf", "title": "can_analyze" }},
+                { "source": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "target": "การประมาณค่าพารามิเตอร์", "data": { "name": "continueFrom", "title": "continue_from" }},
+                { "source": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "target": "การทดสอบสมมติฐาน", "data": { "name": "continueFrom", "title": "continue_from" }},
+                { "source": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "target": "การสุ่มตัวอย่าง", "data": { "name": "continueFrom", "title": "continue_from" }},
+                { "source": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "target": "ระเบียบวิธีการทางสถิติ", "data": { "name": "continueFrom", "title": "continue_from" }},
+                { "source": "ระบบสมการเชิงเส้น", "target": "การวิเคราะห์การถดถอยเชิงเส้นแบบง่ายและการวิเคราะห์สหสัมพันธ์", "data": { "name": "continueFrom", "title": "continue_from" }},
+                { "source": "รูปแบบมาร์คอฟ", "target": "ระบบสมการเชิงเส้น", "data": { "name": "continueFrom", "title": "continue_from" }},
+                { "source": "1141001", "target": "1141001_LO4", "data": { "name": "hasLearningOf", "title": "has_learning_of" }},
+                { "source": "1141001", "target": "1141001_LO3", "data": { "name": "hasLearningOf", "title": "has_learning_of" }},
+                { "source": "1141001", "target": "1141001_LO2", "data": { "name": "hasLearningOf", "title": "has_learning_of" }},
+                { "source": "1141001", "target": "1141001_LO1", "data": { "name": "hasLearningOf", "title": "has_learning_of" }}]}
+        expected_nodes = expected_json["nodes"]
+        expected_edges = expected_json["edges"]
 
-    @classmethod
-    def setUpClass(cls):
-        cls.ontology = get_ontology("file://data/example.owl").load()
-        cls.nodes, cls.links = extract_nodes_and_links(cls.ontology)
 
-    def test_number_of_LearningObjectives(self):
-        count = sum(1 for entry in self.links if entry["source"] == self.source)     
-        # Ensure the count matches the expected value 
-        self.assertEqual(count, 3) 
+        owl_file = load_ontology("file://ambc2/test/data/testCurrOntology.owl")
+        course_id = "1141001"
+        actual_result = extract_nodes_and_links(owl_file, course_id)
+        actual_nodes = actual_result["nodes"]
+        actual_edges = actual_result["edges"]
 
-    def test_match_LinkNames(self):
-        for entry in self.links:
-            if entry["source"] == self.source:
-                # Check if the link name is in the expected list
-                self.assertIn(entry["name"], self.expected_link_names) 
+        self.assertEqual(len(actual_nodes), len(expected_nodes))
+        self.assertEqual(len(actual_edges), len(expected_edges))
 
-if __name__ == '__main__':
-    unittest.main()
+        for node in expected_nodes:
+            self.assertIn(node, actual_nodes)
+        
+        for edge in expected_edges:
+            self.assertIn(edge, actual_edges)
